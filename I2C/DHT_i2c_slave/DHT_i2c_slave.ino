759200632ccc8edc4a165e5170774d85db82397f
sudhakar-pi <52900844+sudhakar-pi@users.noreply.github.com>
2021-07-21 13:37:26 +0530

    Update DHT_i2c_slave.ino

--- I2C/DHT_i2c_slave/DHT_i2c_slave.ino
+++ I2C/DHT_i2c_slave/DHT_i2c_slave.ino
@@ -1,19 +1,8 @@
-//Slave Arduino Programming
-
-//I2C SLAVE CODE
-//I2C Communication between Two Arduino
-//CircuitDigest
-//Pramoth.T
-
-#include<Wire.h>                          //Library for I2C Communication functions
-    //Define LCD Module Pins (RS,EN,D4,D5,D6,D7)
-
+#include<Wire.h>                          
 int SlaveReceived[10];
 int i;
 void setup() 
-
-{
-                            //Clears LCD display
+{                          
   Serial.begin(9600);                     //Begins Serial Communication at 9600 baud rate
   Wire.begin(8);                          //Begins I2C communication with Slave Address as 8 at pin (A4,A5)
   Wire.onReceive(receiveEvent);           //Function call when Slave receives value from master
@@ -22,7 +11,7 @@ void setup()
 
 void loop(void) 
 {
-                        //Prints SlaveReceived value in LCD received from Master
+                        
   Serial.println("temp Received From Master:");   //Prints in Serial Monitor 
   Serial.println(SlaveReceived[0]); 
   Serial.println("humidity Received From Master:");   //Prints in Serial Monitor 
@@ -39,7 +28,7 @@ void receiveEvent (int howMany)                    //This Function is called whe
 int c = Wire.read(); // receive a byte as character
 SlaveReceived[i]=c;
 }
-   //Used to read value received from master and store in variable SlaveReceived
+   
 }
 
 void requestEvent()                                //This Function is called when Master wants value from slave
